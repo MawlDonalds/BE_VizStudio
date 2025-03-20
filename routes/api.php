1a03e0cbf57055d06a410cf876b324c3415f929a
FatahRozaq <fatah.rozaq20@gmail.com>
2025-03-20 11:46:11 +0700

    Perpindahan projek

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Api\ApiGetDataController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('kelola-dashboard')->group(function () {
+    // Route::post('/convert-sql', [ApiKelolaDashboardController::class, 'convertSql']);
+    Route::get('/tables', [ApiGetDataController::class, 'getAllTables']);
+    Route::get('/columns/{table}', [ApiGetDataController::class, 'getTableColumns']);
+    // Route::post('/table-data/{table}', [ApiGetDataController::class, 'getTableDataByColumns']);
+    Route::post('/table-data/{table}', [ApiGetDataController::class, 'getTableDataByColumns']);
+    Route::post('/execute-query', [ApiGetDataController::class, 'executeQuery']);
+
+    // Route::get('/table-data/{table}', [ApiTableDataController::class, 'getTableData']);
+});
