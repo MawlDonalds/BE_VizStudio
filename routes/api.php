2fd56e0a91a526caef23ce5cb45dba69088de1d7
gianvilcanpatra <gianvilcanpatra@gmail.com>
2025-04-07 11:16:39 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,12 @@ Route::prefix('kelola-dashboard')->group(function () {
     Route::get('/tables', [ApiGetDataController::class, 'getAllTables']);
     // Route::post('/tables', [ApiGetDataController::class, 'getAllTables']);
     Route::get('/columns/{table}', [ApiGetDataController::class, 'getTableColumns']);
-    Route::post('/table-data/{table}', [ApiGetDataController::class, 'getTableDataByColumns']);
     Route::post('/execute-query', [ApiGetDataController::class, 'executeQuery']);
 
     Route::post('/fetch-database', [ApiConnectDatabaseController::class, 'connectDB']);
     Route::get('/fetch-table/{id}', [ApiConnectDatabaseController::class, 'fetchTables']);
     Route::get('/fetch-column/{table}', [ApiConnectDatabaseController::class, 'getTableColumns']);
     Route::post('/fetch-data/{table}', [ApiConnectDatabaseController::class, 'getTableDataByColumns']);
+
+    Route::post('/table-data', [ApiGetDataController::class, 'getTableDataByColumns']);
 });
