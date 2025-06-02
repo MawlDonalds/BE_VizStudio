c55c4cd46cf2c3268cd4f87ec1bd2c69932a9142
naufalfad <naufalfad29@gmail.com>
2025-06-02 10:02:32 +0700

    Otentikasi

---
+++ app/Http/Controllers/Api/ApiOtentikasiController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class ApiOtentikasiController extends Controller
+{
+    public function registerUser(Request $request)
+    {
+        try{
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users,email',
+                'password' => 'required|string|min:8|confirmed',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Validasi Gagal',
+                    'errors' => $validator->errors()->all()
+                ], 422);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Data User berhasil disimpan',
+                'data' => [
+                    'user' => [
+                        'id_user' => $user->id_user,
+                        'name' => $user->name,
+                        'email' => $user->email,
+                        'created_time' => $user->created_time,
+                    ],
+                    'token' => $token,
+                    'token_type' => 'Bearer'
+                ]
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'error' => 'Registration failed ' . $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function loginUser(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|email',
+                'password' => 'required|string',
+            ]);
+
+            if ($validator->fails())
+            {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Login Gagal, silahkan coba lagi',
+                    'errors' => $validator->errors()
+                ], 422);
+            }
+
+            $user = User::where('email', $request->email)
+                        ->where('is_deleted', false)
+                        ->first();
+
+            if (!$user || !Hash::check($request->password, $user->password))
+            {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Password salah, silahkan coba lagi'
+                ], 401);
+            }
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Login berhasil',
+                'data' => [
+                    'user' => [
+                        'id_user' => $user->id_user,
+                        'name' => $user->name,
+                        'email' => $user->email
+                    ],
+                    'token' => $token,
+                    'token_type' => 'Bearer'
+                ]
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                    'success' => false,
+                    'message' => 'Login gagal, silahkan coba lagi ' . $e->getMessage()
+            ], 500);
+        }
+    }
+}
